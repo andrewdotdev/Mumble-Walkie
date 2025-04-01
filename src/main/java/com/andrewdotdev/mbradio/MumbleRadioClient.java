902af7ce063df8169ac7e79a56d575d073ce926c
Andrew <109876401+andrewdotdev@users.noreply.github.com>
2025-04-01 18:44:23 +0200

    Initial commit

---
+++ src/main/java/com/andrewdotdev/mbradio/MumbleRadioClient.java
@@ -0,0 +1,10 @@
+package com.andrewdotdev.mbradio;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MumbleRadioClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/andrewdotdev/mbradio/MumbleRadioDataGenerator.java
@@ -0,0 +1,11 @@
+package com.andrewdotdev.mbradio;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MumbleRadioDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
